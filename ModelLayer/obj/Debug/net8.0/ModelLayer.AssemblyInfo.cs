17c03ee751ad93d17f3f4a615134e978c115249d
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-06-24 12:37:31 +0530

    changes made

--- ModelLayer/obj/Debug/net8.0/ModelLayer.AssemblyInfo.cs
+++ ModelLayer/obj/Debug/net8.0/ModelLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe03b6757e2d446a61967d3e757f83668d07977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d925774e960be0ced59692d2eddb86b6cef91774")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
